7bc02b210a45d7f72ac5dc7669e6eb27209e21fc
commoner <timothy.baron@gmail.com>
2015-02-09 21:31:48 -0800

    remove failing connections

--- echo.js
+++ echo.js
@@ -5,15 +5,37 @@ exports.sendMessage = function(message, connections, pendingMessages, newMessage
   var reducedCollection = underscore.reject(connections, function(connection){
     return connection.id === json.id || connection.deactivated;
   });
-  if (reducedCollection.length > 0) {
-    var connection = underscore.sample(reducedCollection);
-    connection.write(JSON.stringify({ message: JSON.parse(message).message }));
-    if (!newMessage) {
-      var index = pendingMessages.indexOf(message);
-      if (index > -1) pendingMessages.splice(index, 1);
+
+  function initiateDelivery() {
+    if (reducedCollection.length > 0) {
+      deliver();
+    }
+    else if (newMessage) {
+      pendingMessages.push(message);
     }
   }
-  else if (newMessage) {
-    pendingMessages.push(message);
+
+  function deliver() {
+    var connection = underscore.sample(reducedCollection);
+    var success = connection.write(JSON.stringify({ message: JSON.parse(message).message }));
+    if (success) {
+      if (!newMessage) {
+        var index = pendingMessages.indexOf(message);
+        if (index > -1) pendingMessages.splice(index, 1);
+      }
+    }
+    else {
+      removeConnection(connection);
+      connection.close();
+      initiateDelivery();
+    }
   }
+
+  function removeConnection(removeMe) {
+    reducedCollection = underscore.reject(reducedCollection, function(connection) {
+      return connection === removeMe;
+    });
+  }
+
+  initiateDelivery();
 }
